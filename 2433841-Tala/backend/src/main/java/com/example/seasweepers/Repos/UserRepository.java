3197bf3dcba8da3787767ae5eceedbf821d8a83d
TSweidan <2433841@brunel.ac.uk>
2026-04-25 21:23:17 +0100

    Add method to find users by country and score

--- 2433841-Tala/backend/src/main/java/com/example/seasweepers/Repos/UserRepository.java
+++ 2433841-Tala/backend/src/main/java/com/example/seasweepers/Repos/UserRepository.java
@@ -7,7 +7,7 @@ import java.util.List;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     List<User> findAllByOrderByTotalScoreDesc();
-
+    List<User> findByCountryIgnoreCaseOrderByTotalScoreDesc(String country);
     List<User> findAllByOrderByWeeklyPointsDesc();
 
 }
